42fef2116b6625e39b586c27bf7f168159ec3e67
ameerthehacker <ameerjhanprof@gmail.com>
2017-09-09 10:55:56 +0530

    Small ui tweaks when no prize is updated

--- resources/views/pages/prizes.blade.php
+++ resources/views/pages/prizes.blade.php
@@ -2,10 +2,20 @@
 
 @section('content')
     @if((Auth::check() && Auth::user()->type == 'admin') || (App\Event::where('show_prize', true)->count() > 0 && App\Prize::all()->count() > 0))
+        @if(App\Prize::all()->count() == 0)
+            <div class="row">
+                <div class="col s12">
+                    <p class="center-align"><i class="fa fa-smile-o" style="font-size: 200px;margin-top:60px;"></i></p>
+                    <h4 class="center-align">
+                        No events are updated with prizes!
+                    </h4>
+                </div>
+            </div> 
+        @endif
         @foreach($events as $event)
             @if(Auth::check() && Auth::user()->type == 'admin')
                 @if($event->hasPrizes())
-                    @include('prizes.partials.prize_details', ['event' => $event])
+                        @include('prizes.partials.prize_details', ['event' => $event])
                 @endif  
             @else
                 @if($event->show_prize && $event->hasPrizes())
